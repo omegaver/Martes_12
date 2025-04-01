c58c4774bc1567df70d781269bf0194db53ba9ee
EGHeWhoRemains <sm75310865@idat.pe>
2025-04-01 09:00:15 -0500

    ProyectoBase

---
+++ src/main/java/pe/edu/idat/demo_git_ramas/DemoGitRamasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_git_ramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitRamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitRamasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/demo_git_ramas/DemoGitRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_git_ramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
